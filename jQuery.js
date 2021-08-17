3d80626f4aac5f1d9e3e8c6cb48b8274f6fc9676
nami73 <73630181+kashimanami@users.noreply.github.com>
2021-08-17 14:15:26 +0900

    Update jQuery.js

--- jQuery.js
+++ jQuery.js
@@ -116,3 +116,26 @@ var syncerSounds = {
 			}
 		}
 })() ;
+
+//fadein
+//ウィンドウの高さを取得する
+var window_h = $(window).height();
+
+//スクロールイベント
+$(window).on("scroll", function() {
+  
+  //スクロールの位置を取得する
+  var scroll_top = $(window).scrollTop();
+ 
+  $(".box").each(function() {
+    //各box要素のtopの位置を取得する
+    var elem_pos = $(this).offset().top;
+    
+    //どのタイミングでフェードインさせるか
+    if (scroll_top >= elem_pos - window_h + 300) {
+      $(this).addClass("fadein");　//特定の位置を超えたらクラスを追加
+    } else {
+      $(this).removeClass("fadein"); //そうでない場合はクラスを削除
+    }
+  });
+});
